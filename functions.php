a6dfa3a1e373208f4c769a742874cc7836946132
Kelly Dwan <ryelle@users.noreply.github.com>
2025-07-08 09:13:58 -0400

    Try a refreshed layout with "About" blurb

--- functions.php
+++ functions.php
@@ -7,6 +7,16 @@
  * @package Ryelle 2022
  */
 
+// Actions & filters.
+add_action( 'after_setup_theme', 'ryelle_2022_setup' );
+add_action( 'wp_enqueue_scripts', 'ryelle_2022_assets' );
+add_action( 'admin_init', 'ryelle_2022_editor_styles' );
+add_action( 'wp_enqueue_scripts', 'ryelle_2022_dequeue_dashicons' );
+add_action( 'render_block_core/comment-template', 'ryelle_2022_render_no_comments' );
+add_action( 'wp_head', 'ryelle_2022_preload_assets' );
+add_filter( 'render_block_data', __NAMESPACE__ . '\update_navigation_items', 10, 3 );
+add_filter( 'render_block_data', __NAMESPACE__ . '\update_site_title', 10, 3 );
+
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
@@ -40,7 +50,6 @@ function ryelle_2022_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'ryelle_2022_setup' );
 
 /**
  * Enqueue the assets.
@@ -54,14 +63,6 @@ function ryelle_2022_assets() {
 
 	wp_enqueue_style( 'ryelle-2022-style' );
 }
-add_action( 'wp_enqueue_scripts', 'ryelle_2022_assets' );
-
-add_action(
-	'mkaz_prism_css_path',
-	function() {
-		return '/build/syntax.css';
-	}
-);
 
 /**
  * Enqueue the editor assets.
@@ -74,7 +75,6 @@ function ryelle_2022_editor_styles() {
 		)
 	);
 }
-add_action( 'admin_init', 'ryelle_2022_editor_styles' );
 
 /**
  * Remove dashicons in frontend for unauthenticated users.
@@ -84,7 +84,6 @@ function ryelle_2022_dequeue_dashicons() {
 		wp_deregister_style( 'dashicons' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'ryelle_2022_dequeue_dashicons' );
 
 /**
  * Comments template block: Add a message when no comments are found.
@@ -98,7 +97,6 @@ function ryelle_2022_render_no_comments( $block_content ) {
 	}
 	return $block_content;
 }
-add_action( 'render_block_core/comment-template', 'ryelle_2022_render_no_comments' );
 
 /**
  * Preload the web fonts to improve performance.
@@ -120,11 +118,79 @@ function ryelle_2022_preload_assets() {
 		<?php
 	}
 }
-add_action( 'wp_head', 'ryelle_2022_preload_assets' );
+
+/**
+ * Update the inner blocks (menu items) in a navigation block.
+ *
+ * @param array         $parsed_block An associative array of the block being rendered.
+ * @param array         $source_block An un-modified copy of `$parsed_block`, as it appeared in the source content.
+ * @param WP_Block|null $parent_block If this is a nested block, a reference to the parent block.
+ */
+function update_navigation_items( $parsed_block, $source_block, $parent_block ) {
+	if ( $parsed_block['blockName'] !== 'core/navigation-link' ) {
+		return $parsed_block;
+	}
+
+	$attributes = $parsed_block['attrs'];
+	if (
+		$attributes['kind'] === 'custom' &&
+		! empty( $attributes['url'] ) &&
+		\str_starts_with( $attributes['url'], '/' )
+	) {
+		$page = get_page_by_path( $attributes['url'] );
+		if ( $page && $page->post_status === 'publish' ) {
+			$attributes['kind'] = 'post-type';
+			$attributes['type'] = 'page';
+			$attributes['id'] = $page->ID;
+			$attributes['url'] = get_permalink( $page->ID );
+		} else {
+			$attributes['url'] = home_url( $attributes['url'] );
+		}
+
+		if ( isset( get_queried_object()->post_type ) && in_array( get_queried_object()->post_type, [ 'work', 'post' ], true ) ) {
+			$queried_archive_link = get_post_type_archive_link( get_queried_object()->post_type );
+			if ( $attributes['url'] === $queried_archive_link ) {
+				$attributes['className'] = 'current-menu-item';
+			}
+		}
+
+		$parsed_block['attrs'] = $attributes;
+	}
+	return $parsed_block;
+}
+
+/**
+ * Switch to paragraph tags on interior pages.
+ *
+ * @param array         $parsed_block An associative array of the block being rendered.
+ * @param array         $source_block An un-modified copy of `$parsed_block`, as it appeared in the source content.
+ * @param WP_Block|null $parent_block If this is a nested block, a reference to the parent block.
+ */
+function update_site_title( $parsed_block, $source_block, $parent_block ) {
+	if ( $parsed_block['blockName'] !== 'core/site-title' ) {
+		return $parsed_block;
+	}
+	if ( is_home() ) {
+		$parsed_block['attrs']['level'] = 1;
+		if ( ! is_paged() ) {
+			$parsed_block['attrs']['isLink'] = false;
+		}
+	} else {
+		$parsed_block['attrs']['level'] = 0;
+	}
+	return $parsed_block;
+}
 
 add_filter(
 	'syntax_highlighting_code_block_auto_detect_languages',
 	function() {
 		return [ 'css', 'scss', 'php', 'html', 'javascript' ];
+	}
+);
+
+add_action(
+	'mkaz_prism_css_path',
+	function() {
+		return '/build/syntax.css';
 	}
 );
